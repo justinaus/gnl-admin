8bed7685137f4c68d1809b3ebf7f79feec3e8415
Bonkook Koo <justinaus2k@gmail.com>
2022-07-11 15:46:18 +0900

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import AppRoutes from "./Routes";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">11</header>
+      <AppRoutes />
     </div>
   );
 }
---
+++ src/Routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import { RoutePath } from "./helpers/routePath";
+import Restaurant from "./pages/restaurants/Restaurant";
+import Restaurants from "./pages/restaurants/Restaurants";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path={RoutePath.Restaurants} element={<Restaurants />} />
+      <Route path={`${RoutePath.Restaurants}/:id`} element={<Restaurant />} />
+      <Route path="*" element={<Navigate to={RoutePath.Restaurants} />} />
+    </Routes>
+  );
+}
