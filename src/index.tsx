81816d0bff2290444c76a1656a794a679c4ead4d
Bonkook Koo <justinaus2k@gmail.com>
2022-07-11 23:45:27 +0900

    Update React.StrictMode 주석 처리

--- src/index.tsx
+++ src/index.tsx
@@ -12,12 +12,13 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>,
+  // 이유는 잘 모르겠는데, 2번 렌더 해서 끈다.
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+  // </React.StrictMode>,
 );
