8e966aa89f8a29338439cef891668ad7c5853894
Bonkook Koo <justinaus2k@gmail.com>
2022-07-13 01:11:56 +0900

    Fix firestore getdoc

--- src/components/restaurants/write/useRestaurant.ts
+++ src/components/restaurants/write/useRestaurant.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore/lite';
+import { doc, getDoc } from 'firebase/firestore/lite';
 import { useCallback, useEffect, useState } from 'react';
 
 import { db } from '../../../lib/firebase';
@@ -8,25 +8,18 @@ export default function useRestaurant(id: string) {
   const [data, setData] = useState<Restaurant | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  const fetch = useCallback(() => {
-    const restaurantsCol = collection(db, 'restaurants');
+  const fetch = useCallback(async () => {
+    const docRef = doc(db, 'restaurants', id);
+    const docSnap = await getDoc(docRef);
 
-    getDocs(restaurantsCol)
-      .then((restaurantsSnapshot) => {
-        const finded = restaurantsSnapshot.docs.find((doc) => doc.id === id);
-
-        if (!finded) {
-          throw new Error('no id');
-        }
-
-        setData({
-          ...finded.data(),
-          id: id,
-        } as Restaurant);
-      })
-      .catch(() => {
-        setError('error');
-      });
+    if (docSnap.exists()) {
+      setData({
+        ...docSnap.data(),
+        id: id,
+      } as Restaurant);
+    } else {
+      setError('error');
+    }
   }, [id]);
 
   useEffect(() => {
